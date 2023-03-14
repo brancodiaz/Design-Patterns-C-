06778838cb2456f703e03f505486816585b9dc52
brancodc <branco1125@gmail.com>
2023-03-14 16:33:51 -0500

    Adding Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Singleton.Singleton firstInstance = Singleton.Singleton.getInstance;
+
+        Console.WriteLine($"Counter: {firstInstance.addCounter().ToString()}"); // Counter: 1
+        Console.WriteLine($"Counter: {firstInstance.addCounter().ToString()}"); // Counter: 2
+        Console.WriteLine($"Counter: {firstInstance.addCounter().ToString()}"); // Counter: 3
+
+        Singleton.Singleton secondInstance = Singleton.Singleton.getInstance;
+
+        //since we are reusing the same instance from Singleton class then the value of Counter now is going to be 4
+        Console.WriteLine($"Counter: {secondInstance.addCounter().ToString()}"); // Counter: 4
+
+        //now wheter we use the first or the second instance the value of Counter will be 5
+        Console.WriteLine($"Counter: {firstInstance.addCounter().ToString()}"); // Counter: 5
+
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private readonly static Singleton _uniqueInstance = new Singleton();
+
+        private int _counter = 0;
+        private Singleton() {}
+        public static Singleton getInstance
+        {
+            get{
+                return _uniqueInstance;
+            }
+        }
+
+        public int addCounter()
+        {
+            _uniqueInstance._counter += 1;
+            return _counter;
+        }
+    }
+}
